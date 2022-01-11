848e776e5d6dd199c667b921445568b867a8e3c3
TJP <eleminati3.0@gmail.com>
2022-01-11 23:52:54 +0100

    1

--- testclient.py
+++ testclient.py
@@ -14,20 +14,3 @@ def connect(i):
     print("C1")
     print('Received', repr(data))
     print("C2")
-
-#for i in range(3):
-#    client_thread = threading.Thread(target=connect, args=(i,))
-
-
-
-#   import socket
-#   
-#   HOST = '127.0.0.1'  # The server's hostname or IP address
-#   PORT = 65432        # The port used by the server
-#   
-#   with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-#       s.connect((HOST, PORT))
-#       s.sendall(b'Hello, world')
-#       data = s.recv(1024)
-#   
-#   print('Received', repr(data))
