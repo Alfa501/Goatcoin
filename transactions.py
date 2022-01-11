6aa5844970d6b9683e6acadf0376352212f0ea9b
unknown <eleminati3.0@gmail.com>
2022-01-11 17:48:35 +0100

    test

--- transactions.py
+++ transactions.py
@@ -45,6 +45,7 @@ def transaction(sender, receiver, amount, balance):
         cc.createBlock(sender, receiver, int(amount))
     elif int(balance) < int(amount):
         print("[*] Not enough GTC for this transaction")
+        
 
 def checkAuth(publicKey, privateKey):
     publicKeyGenerated = hashlib.sha256(str(privateKey).strip().encode()).hexdigest()
@@ -52,32 +53,32 @@ def checkAuth(publicKey, privateKey):
     if publicKeyGenerated == publicKey:
         return True
 
-while True:
-    selection = input("[*] Get Balance: 1\n[*] Make Transaction: 2\n[*] Create Wallet: 3\n")
-    if selection == "1":
-        balance = getBalance(input("[*] Please enter your public key: "))
-        print(balance)
-
-    elif selection == "2":
-        sender = input("[*] Sender Public Key: ")
-        receiver = input("[*] Receiver Public Key: ")
-        amount = input("[*] Enter amount of GTC to be sent: ")
-        auth = input("[*] Sender Private Key ")
-
-        if checkAuth(sender, auth) == True:
-            currentBalance = getBalance(sender)
-            print("[*] Your current balance is: {} GTC".format(currentBalance))
-
-            if input("[*] Do you want to continue? y/n") == "y":
-                transaction(sender, receiver, amount, currentBalance)
-                print("[*] Your current balance is: {} GTC".format(getBalance(sender)))
-
-            else:
-                pass
-
-        else:
-            print("[*] Incorrect Key")
-    elif selection == "3":
-        createWallet()
-    else:
-        print("[*] Please choose one of the above options")
+#   while True:
+#       selection = input("[*] Get Balance: 1\n[*] Make Transaction: 2\n[*] Create Wallet: 3\n")
+#       if selection == "1":
+#           balance = getBalance(input("[*] Please enter your public key: "))
+#           print(balance)
+#   
+#       elif selection == "2":
+#           sender = input("[*] Sender Public Key: ")
+#           receiver = input("[*] Receiver Public Key: ")
+#           amount = input("[*] Enter amount of GTC to be sent: ")
+#           auth = input("[*] Sender Private Key ")
+#   
+#           if checkAuth(sender, auth) == True:
+#               currentBalance = getBalance(sender)
+#               print("[*] Your current balance is: {} GTC".format(currentBalance))
+#   
+#               if input("[*] Do you want to continue? y/n") == "y":
+#                   transaction(sender, receiver, amount, currentBalance)
+#                   print("[*] Your current balance is: {} GTC".format(getBalance(sender)))
+#   
+#               else:
+#                   pass
+#   
+#           else:
+#               print("[*] Incorrect Key")
+#       elif selection == "3":
+#           createWallet()
+#       else:
+#           print("[*] Please choose one of the above options")
